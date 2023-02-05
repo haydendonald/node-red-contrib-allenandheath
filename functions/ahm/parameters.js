1615bcc7f65cf69e308cc7c94d6eadd188709cd3
Nathaniel Chang <changipro.nathaniel@gmail.com>
2023-02-05 22:31:09 +1030

    adding the faderlevel access

--- functions/ahm/parameters.js
+++ functions/ahm/parameters.js
@@ -147,7 +147,7 @@ module.exports = {
             sendPing: require("./sendPing.js").object()
             // channelName: require("./channelName.js").object(),
             // muteControl: require("./muteControl.js").object(),
-            // faderLevel: require("./faderLevel.js").object(),
+            faderLevel: require("./faderLevel.js").object(),
             // sceneRecall: require("./sceneRecall.js").object()
         },
 
