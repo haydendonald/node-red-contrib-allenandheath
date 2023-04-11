727657d9bd4f768e93c6625222b6198dde76137e
Hayden <haydendonald@gmail.com>
2023-04-11 19:08:06 +0930

    Added fader level

--- AHM.md
+++ AHM.md
@@ -53,7 +53,6 @@ var msg = {
 
 ### Example request to the console
 ```
-//Request to set the mute to on for channel 1
 var msg = {
     "payload": {
         "function": "muteControl",
@@ -74,128 +73,50 @@ var msg = {
 ```
 
 
-## Fader Level (Get/Set)
-Sets/Gets the channel fader level
-- `function` "faderLevel"
-- `inputChannel` which is an array containing each input
-
-- `channelType` The channel type to set
-- `channel` The channel id
-- `level` The level of the fader (0-100%)
+## Fader Level (faderLevel) (Get/Set)
+Sets/Gets the level of a channel
 
 ### Example message from the console
 ```
 var msg = {
     "payload": {
         "function": "faderLevel",
-        "inputChannel": {
-            1: 100,
-            2:  0
+        "channel": {
+            1: 0-127
+            2: 0-127
+            ...
+        },
+        "zone": {
+            1: 0-127
+            2: 0-127
+            ...
+        },
+        "controlGroup": {
+            1: 0-127
+            2: 0-127
+            ...
         }
-        ...
     }
 }
 ```
 
 ### Example request to the console
 ```
-//Request to set the channel level
 var msg = {
     "payload": {
         "function": "faderLevel",
-        "channelType": "inputChannel",
-        "channel": "1",
-        "level": 100
+        "channelSelection": "channel/zone/controlGroup",
+        "channel": 1,
+        "level": 0-127
     }
 }
 ```
 
 ```
-//Request to get channel levels
+//Request to get channels
 var msg = {
     "payload": {
         "function": "faderLevel"
-    }
-}
-```
-
-## Channel Name (Get/Set)
-Sets/Gets the channel name
-- `function` "channelName"
-- `inputChannel` Array of the channels
-
-- `channelType` The channel type to set
-- `channel` The channel id
-- `name` The string to set the channel name to
-
-### Example message from the console
-```
-var msg = {
-    "payload": {
-        "function": "channelName",
-        "inputChannel": {
-            1: "Hello World!",
-            2: "Hello World!"
-        }
-    }
-}
-```
-
-### Example request to the console
-```
-//Request to set the channel name
-var msg = {
-    "payload": {
-        "function": "channelName",
-        "channelType": "inputChannel",
-        "channel": "1",
-        "name": "Nice"
-    }
-}
-```
-
-```
-//Request to get channel names
-var msg = {
-    "payload": {
-        "function": "channelName"
-    }
-}
-```
-
-## Scene Recall (Get/Set)
-Recalls a scene
-- `function` "sceneRecall"
-- `currentScene` The current scene
-
-- `scene` The scene id to recall
-
-### Example message from the console
-```
-var msg = {
-    "payload": {
-        "function": "sceneRecall",
-        "currentScene": 1
-    }
-}
-```
-
-### Example request to the console
-```
-//Request to recall a scene
-var msg = {
-    "payload": {
-        "function": "sceneRecall",
-        "scene": 2
-    }
-}
-```
-
-```
-//Request to get the current scene
-var msg = {
-    "payload": {
-        "function": "sceneRecall"
     }
 }
 ```
--- functions/ahm/faderLevel.js
+++ functions/ahm/faderLevel.js
@@ -1,148 +1,153 @@
 module.exports = {
-    object: function() { return {
-        parameters: undefined,
-        data: {},
+    object: function () {
+        return {
+            parameters: undefined,
+            data: {
+                "channel": {},
+                "zone": {},
+                "controlGroup": {}
+            },
 
-        setup: function(parameters) {
-            this.parameters = parameters;
-        },
+            setup: function (parameters) {
+                this.parameters = parameters;
+            },
 
-        reset: function() {
-            data = {};
-        },
+            reset: function () {
+                data = {
+                    "channel": {},
+                    "zone": {},
+                    "controlGroup": {}
+                }
+            },
 
-        //Send out
-        generatePacket: function generatePacket(msg, server, midiChannel, callback) {
-            var temp = this;
-            if(msg.payload.function == "faderLevel") {
-                if(msg.payload.channelType === undefined && msg.payload.channel === undefined && msg.payload.level === undefined) {
-                    //Just return the stored information
+            //Request all the channels
+            initial: function (server, midiChannel) {
+                var self = this;
+
+                for (var i = 0; i < 3; i++) {
+                    for (var j = 0; j < self.parameters.totalChannelSelection[i]; j++) {
+                        var packet = Buffer.concat([Buffer.from(self.parameters.sysexHeader.allCall), Buffer.from([i, 0x01, 0x0B, 0x17, j, 0xF7])]);
+                        try { server.write(packet); }
+                        catch (e) { console.log("Failed to send packet! "); console.log(e); return false; }
+                    }
+                }
+            },
+
+            //Send out
+            generatePacket: function generatePacket(msg, server, midiChannel, callback) {
+                var object = this;
+                if (msg.payload.function == "faderLevel") {
+                    if (msg.payload.channelSelection === undefined && msg.payload.channel === undefined && msg.payload.level === undefined) {
+                        //Just return the stored information
+                        var msg = {
+                            "payload": {
+                                "function": "faderLevel"
+                            }
+                        }
+
+                        Object.assign(msg.payload, object.data);
+
+                        callback(msg);
+                        return true;
+                    }
+                    else {
+                        if (msg.payload === undefined) { return "payload is not specified"; }
+                        if (msg.payload.channelSelection === undefined) { return "channelSelection is not specified"; }
+                        if (msg.payload.channel === undefined) { return "channel is not specified"; }
+                        if (msg.payload.level === undefined) { return "muted is not specified"; }
+
+                        //Validate the channel selection
+                        var channel = parseInt(msg.payload.channel);
+                        var level = parseInt(msg.payload.level);
+                        var channelSelectionIdx = {
+                            "channel": 0,
+                            "zone": 1,
+                            "controlGroup": 2
+                        }[msg.payload.channelSelection];
+                        if (channelSelectionIdx === undefined) { return "Invalid channel selection"; }
+
+                        //Validate the channel
+                        if (channel < 1 || channel > object.parameters.totalChannelSelection[channelSelectionIdx] + 1) {
+                            return "Invalid channel";
+                        }
+
+                        //Validate the level
+                        if(level < 0 || level > 0x7F) {
+                            return "Invalid level";
+                        }
+
+                        //Generate the packet!
+                        var retMsg = {
+                            "payload": {
+                                "function": "faderLevel"
+                            }
+                        }
+                        try {
+                            object.data[msg.payload.channelSelection][channel] = msg.payload.level;
+                            Object.assign(retMsg.payload, object.data);
+                            callback(retMsg);
+                        }
+                        catch (e) { }
+
+                        var packet = Buffer.from([(0xB0 + channelSelectionIdx), 0x63, channel - 1, (0xB0 + channelSelectionIdx), 0x62, 0x17, (0xB0 + channelSelectionIdx), 0x06, level]);
+                        return packet;
+                    }
+                }
+            },
+
+            //Recieved data
+            recieve: function recieve(midiChannel, data, server, syncActive) {
+                var object = this;
+                var updated = false;
+
+                for (var i = 0; i < data.length;) {
+                    if (data[i + 1] != 0x63) { break; }
+                    if (data[i + 3] != 0x62) { break; }
+                    if (data[i + 4] != 0x17) { break; }
+                    if (data[i + 5] != 0x06) { break; }
+
+                    var channelSelection = data[i + 0] - 0xB0;
+                    var channel = data[i + 2];
+                    var level = data[i + 6];
+
+                    if (channelSelection < 0 || channelSelection > 2) { break; }
+                    if (channel < 0 || channel > object.parameters.totalChannelSelection[channelSelection]) { break; }
+                    object.data[Object.keys(object.data)[channelSelection]][channel + 1] = level;
+                    data = data.slice(i + 7, data.length);
+                    i = 0;
+                    updated = true;
+                }
+
+                if (updated == true) {
                     var msg = {
                         "payload": {
                             "function": "faderLevel"
                         }
                     }
-                    
-                    Object.assign(msg.payload, temp.data);
 
-                    callback(msg);
-                    return true;
+                    Object.assign(msg.payload, object.data);
+                    return msg;
                 }
-                else {
-                    var channelId = -1;
-                    if(msg.payload.rawChannel !== undefined) {
-                        channelId = parseInt(msg.payload.rawChannel);
-                    }
-                    else {
-                        if(msg.payload.channelType === undefined){return "channelType is not specified";}
-                        if(msg.payload.channel === undefined){return "channel is not specified";}
 
-                        //Find the channel id
-                        channelId = temp.parameters.channelTypes[msg.payload.channelType][msg.payload.channel];
-                        if(channelId === undefined){return "channel id was not found";}
+                return true;
+            },
 
-                    }
-                    if(msg.payload.level === undefined){return "level is not specified";}
-                    var level = (parseFloat(msg.payload.level) / 100) * 127;
-
-                    //Generate the packet!
-                    var retMsg = {
-                        "payload": {
-                            "function": "faderLevel"
-                        }
-                    }
-                    
-                    try {
-                        temp.data[msg.payload.channelType][msg.payload.channel] = msg.payload.level;
-                        Object.assign(retMsg.payload, temp.data);
-                        callback(retMsg);
-                    }
-                    catch(e){}
-
-                    var NRPNMSB = Buffer.from([(0xB0 + parseInt(midiChannel, 16)), 0x63, channelId]);
-                    var NRPNLSB = Buffer.from([(0xB0 + parseInt(midiChannel, 16)), 0x62, 0x17]);
-                    var dataMSB = Buffer.from([(0xB0 + parseInt(midiChannel, 16)), 0x06, level]);
-                    var dataLSB = Buffer.from([(0xB0 + parseInt(midiChannel, 16)), 0x26, 0x07]);
-
-                    return Buffer.concat([NRPNMSB, NRPNLSB, dataMSB, dataLSB]);
-                }
-            }
-
-            return false;
-        },
-
-        //Recieved data
-        recieve: function recieve(midiChannel, data, server, syncActive) {
-            var object = this;
-
-            var ret = false;
-            for(var i = 0; i < data.length; i++) {
-                if(data[i + 0] == (0xB0 + parseInt(midiChannel, 16)) && data[i + 3] == (0xB0 + parseInt(midiChannel, 16)) && data[i + 6] == (0xB0 + parseInt(midiChannel, 16)) && data[i + 9] == (0xB0 + parseInt(midiChannel, 16))) {
-                    if(data[i + 1] == 0x63 && data[i + 4] == 0x62 && data[i + 7] == 0x06 && data[i + 10] == 0x26 && data[i + 11] == 0x07) {
-                        //Valid NRPN command
-
-                        //Fader
-                        if(data[i + 5] == 0x17) {
-                            if(object.data["inputChannel"] === undefined){
-                                object.data["inputChannel"] = {};
-                            }
-
-                            //Find the channe;
-                            var channelType = undefined;
-                            var channel = undefined;
-                            Object.keys(object.parameters.channelTypes).forEach(function(key) {
-                                Object.keys(object.parameters.channelTypes[key]).forEach(function(key2) {
-
-                                    if(parseInt(object.parameters.channelTypes[key][key2]) == parseInt(data[i + 2])) {
-                                        channelType = key;
-                                        channel = key2;
-                                    }
-                                });
-                            });
-
-                            if(object.data[channelType] === undefined){
-                                object.data[channelType] = {};
-                            }
-                            object.data[channelType][channel] = (data[i + 8] / 127) * 100;
-
-                            ret = true;
-                        }
-                    }
-                }
-            }
-
-            //If we found something and sync is not active send it out!
-            if(syncActive == false && ret == true) {
+            //Send the data
+            getData() {
                 var msg = {
                     "payload": {
-                        "function": "faderLevel"
+                        "function": "muteControl"
                     }
                 }
-                
-                Object.assign(msg.payload, object.data);
 
+                Object.assign(msg.payload, this.data);
                 return msg;
+            },
+
+            //Ping
+            sendPing: function sendPing(server, midiChannel, successFunction) {
+                return false;
             }
-            else if(ret == true){return true;}
-
-            return false;
-        },
-
-        //Send the data
-        getData() {
-            var msg = {
-                "payload": {
-                    "function": "faderLevel"
-                }
-            }
-            
-            Object.assign(msg.payload, this.data);
-            return msg;
-        },
-
-        //Ping
-        sendPing: function sendPing(server, midiChannel, successFunction) {
-            return false;
         }
-    }}
+    }
 }
